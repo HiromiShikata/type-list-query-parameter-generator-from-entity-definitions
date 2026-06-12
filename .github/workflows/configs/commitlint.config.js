a8aed834a6f8816bc36138429e7af287cb265128
GitHub Action <action@github.com>
2026-06-12 16:12:43 +0000

    autogen: updated common files in HiromiShikata

--- .github/workflows/configs/commitlint.config.js
+++ .github/workflows/configs/commitlint.config.js
@@ -1,3 +1,8 @@
+// DO NOT EDIT DIRECTLY.
+// This file is auto-synchronized from HiromiShikata/repositories-management.
+// Direct edits in downstream repositories will be overwritten by the next sync.
+// Update the source file in HiromiShikata/repositories-management instead.
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
